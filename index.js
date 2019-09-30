6e08d37caebf0c313c5cc1ae921fac16573d710e
IslaMcN <51002520+IslaMcN@users.noreply.github.com>
2019-09-30 12:59:13 -0700

    having problems with get by id and post

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // implement your API here
+const express = require('express');
+const server = express();
+const database = require('./data/db')
+server.get('/api/users', (req, res) => {
+    database
+    .find()
+    .then(data => {
+        res.send(data);
+    })
+    .catch(error => {
+        res.send(error);
+    });
+});
+
+server.get('/api/users/:id', (req, res) => {
+    const id = req.params.id;
+    database
+    .find(id)
+    .then(data => {
+        res.send(data);
+    })
+    .catch(error => {
+        res.send(error);
+    });
+});
+
+server.post('/api/users', (req, res) => {
+    const data = req.body;
+    console.log('data', data);
+    database
+    .add(data)
+    .then(dat => {
+        res.json({message: 'error saving the data'});
+    });
+});
+
+const port = 5000;
+server.listen(port, () => console.log(`\n**API on Port ${port}**\n`));
